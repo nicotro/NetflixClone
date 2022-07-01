5665d9e0f30b3f0b671cee782999cbadfb731a4d
nicotro <ntroccaz@gmail.com>
2022-07-01 16:08:35 +0200

    BG & logo resources added

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { HomeContainer } from './containers/home-container';
-
+import './style/index.css';
+import App from './App.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomeContainer />
+    <App />
   </React.StrictMode>
 );
 
