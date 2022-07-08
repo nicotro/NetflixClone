f8ed94837bb19806ee863512607cd2f6689702dd
Kevin Ronsse <103761732+KevinRonsse@users.noreply.github.com>
2022-07-08 16:52:52 +0200

    footer

--- front/src/containers/home-container.jsx
+++ front/src/containers/home-container.jsx
@@ -3,14 +3,17 @@ import { HomeAd1Section } from "../components/home-ad1-section";
 import { HomeAd2Section } from "../components/home-ad2-section";
 import { HomeFaqSection } from "../components/home-faq-section";
 import "./../style/home-container.css";
+import { Footer } from "../components/footer";
 
 export function HomeContainer() {
-  return (
-    <div>
-      <Header />
-      <HomeAd1Section />
-      <HomeAd2Section />
-      <HomeFaqSection />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <HomeAd1Section />
+            <HomeAd2Section />
+            <HomeFaqSection />
+            <Footer/>
+            
+        </div>
+    );
 }
