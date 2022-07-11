74862cecb9f1ad295830c2c454907b4a17c7ce5a
nicotro <ntroccaz@gmail.com>
2022-07-11 13:38:12 +0200

    NetflixClone back classes creation

---
+++ back/NetflixCloneEntity/Classes/Faq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixCloneEntity.Classes
+{
+    public class Faq
+    {
+        private int id;
+        private string question;
+        private string answer;
+
+        public Faq()
+        {
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Question { get => question; set => question = value; }
+        public string Answer { get => answer; set => answer = value; }
+    }
+}
---
+++ back/NetflixCloneEntity/Classes/Resource_Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixCloneEntity.Classes
+{
+    public class Resource_Category
+    {
+        private int id;
+        private string resource_id;
+        private string category_id;
+
+        public int Id { get => id; set => id = value; }
+        public string Resource_id { get => resource_id; set => resource_id = value; }
+        public string Category_id { get => category_id; set => category_id = value; }
+    }
+}
