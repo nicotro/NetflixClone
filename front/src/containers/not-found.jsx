eb77763d5f6ce3aaa9b2a6ab6d13a5dd8b29d3db
nicotro <ntroccaz@gmail.com>
2022-07-22 01:48:00 +0200

    NetflixClone 404 update OK

--- front/src/containers/not-found.jsx
+++ front/src/containers/not-found.jsx
@@ -13,29 +13,24 @@ export function NotFound() {
           </Link>
         </div>
       </div>
-      <div className="not-found-content" style={{ backgroundImage: `url(${backgroundImage})`}}>
-        <h1 className="not-found-content-title">Lost your way?</h1>
-        <div className="not-found-content-body">
-          <p>
-          Sorry, we can't find that page. You'll find lots to explore on the home page. 
-          </p>
-          <div className="not-found-content-message">
-
+      <div className="not-found-content" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="not-found-content-message">
+          <h1 className="not-found-content-title">Lost your way?</h1>
+          <div className="not-found-content-body">
+            <p>
+              Sorry, we can't find that page. You'll find lots to explore on the home page.
+            </p>
+            <Link to={"/"} className="not-found-content-button">
+              Netflix Home
+            </Link>
+            <div className="not-found-content-404">
+              <span>
+                Code d'erreur&nbsp;: <strong>NSES-404</strong>
+              </span>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    // <div
-    //   className="notfound-background"
-    //   style={{ backgroundImage: `url(${backgroundImage})` }}
-    // >
-    //   <div>
-    //     <div className="notfound-message">
-    //       <h1 className="h1-404">404</h1>
-    //       <img src={logo} className="tmp-logo" alt="logo" />
-    //       <h1 className="h1-notfound">page Not found</h1>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
