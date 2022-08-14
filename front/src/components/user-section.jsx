b6faaaf3a68d6170cee82ac6fac2d6576b5839df
Nicolas <ntroccaz@gmail.com>
2022-08-14 17:02:10 +0200

    NetFlixClone front resources sliders display & selection toggle

--- front/src/components/user-section.jsx
+++ front/src/components/user-section.jsx
@@ -1,7 +1,13 @@
+import { UserResourceSlider } from "./user-resource-slider";
+
 export function UserSection({ category }) {
     return (
-        < div className="debug-white-text">
-            <p>Category = {category}</p>
-        </div>
+        <>
+            < div className="debug-white-text">
+                <p>Category = {category}</p>
+                <UserResourceSlider genreId="Genre #1" />
+                <UserResourceSlider genreId="Genre #2" />
+            </div>
+        </>
     );
 }
